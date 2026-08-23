cd19410b3f39bd0f6a058522d549ca7915216660
Wesley Souza <wesley.tech015@gmail.com>
2026-08-23 18:02:36 -0300

    chore: criar projeto inicial da calculadora

---
+++ app/src/main/java/com/wesley/calculadoramobile/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.wesley.calculadoramobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.wesley.calculadoramobile.ui.theme.CalculadoraMobileTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            CalculadoraMobileTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { espacamentoInterno ->
+
+                    TelaCalculadora(
+                        modifier = Modifier.padding(espacamentoInterno)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TelaCalculadora(modifier: Modifier = Modifier) {
+    Text(
+        text = "Calculadora Mobile",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviaCalculadora() {
+    CalculadoraMobileTheme {
+        TelaCalculadora()
+    }
+}
